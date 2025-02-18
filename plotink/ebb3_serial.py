7ca66f43610c3c364c1397cb76a04ea07d2bda28
Anna S Berleant <berleant@github.com>
2025-02-18 17:40:20 -0500

    fixup! wait until readline sends back \n

--- plotink/ebb3_serial.py
+++ plotink/ebb3_serial.py
@@ -408,7 +408,7 @@ class EBB3:
         # and wait for a response
         response = ""
         n_retry_count = 0
-        while response[-1] != "\n"  and n_retry_count < self.readline_retry_max:
+        while (len(response) == 0 or response[-1] != "\n") and n_retry_count < self.readline_retry_max:
             # get new response to replace null response if necessary
             response = response + self.port.readline().decode('ascii').strip()
             n_retry_count += 1
