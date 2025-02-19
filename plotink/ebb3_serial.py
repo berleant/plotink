a7af76cf3adb7c20b5fadba284d95cba38b3602b
Anna S Berleant <berleant@github.com>
2025-02-19 13:37:02 -0500

    fixup! try to get any kind of response

--- plotink/ebb3_serial.py
+++ plotink/ebb3_serial.py
@@ -417,7 +417,7 @@ class EBB3:
         responses = []
         n_retry_count = 0
         # poll for response until we get any response and self.port indicates there is no more input, a maximum of self.readline_retry_max times  # TODO adjust/tune retries and timeout params
-        while (len(response) == 0 or self.port.in_waiting > 0) and n_retry_count < self.readline_retry_max:
+        while (len(responses) == 0 or self.port.in_waiting > 0) and n_retry_count < self.readline_retry_max:
             responses += self.port.readlines()
             n_retry_count += 1
 
